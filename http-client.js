a8f826c82c0081f9fef831497a6d66f7822c3b5b
marianalopesdev <marianalopes.dev@gmail.com>
2023-04-19 01:26:01 -0300

    httpclient completed

--- http-client.js
+++ http-client.js
@@ -1,3 +1,22 @@
-const http = require('http');
-const file = process.argv[2];
-console.log(file);
+let http = require('http')
+const file =process.argv[2];
+
+
+http.get(file, function (response) {
+  response.setEncoding('utf8')
+  response.on('data', console.log)
+  response.on('error', console.error)
+})
+
+// Here's the official solution in case you want to compare notes:
+
+// ─────────────────────────────────────────────────────────────────────────────
+
+//     'use strict'
+//     const http = require('http')
+
+//     http.get(process.argv[2], function (response) {
+//       response.setEncoding('utf8')
+//       response.on('data', console.log)
+//       response.on('error', console.error)
+//     }).on('error', console.error)
